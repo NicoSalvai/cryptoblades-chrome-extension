068a368e71fcd8e67f47c1074b4f53232657be37
Nicolas Salvai <nicolas-salvai@hotmail.com>
2021-08-09 15:45:30 -0300

    bladesknight

--- background.js
+++ background.js
@@ -9,7 +9,7 @@ chrome.storage.local.get("name", data => {
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if(changeInfo.status === "complete" && tab.url.includes("https://app.cryptoblades.io/#/")) {
+    if(changeInfo.status === "complete" && (tab.url.includes("https://app.cryptoblades.io/#/") || tab.url.includes("https://bladeknight.io/#/"))) {
         chrome.scripting.executeScript({
             target: {tabId: tabId},
             files: ["./foreground.js"]
